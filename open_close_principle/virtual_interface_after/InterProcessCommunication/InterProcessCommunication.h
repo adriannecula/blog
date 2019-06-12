3852b008fed1e944b4241d10d60a3aae1506725b
adriannecula <adrian.necula@live.com>
2019-06-12 19:20:28 +0300

    improve formating

--- open_close_principle/virtual_interface_after/InterProcessCommunication/InterProcessCommunication.h
+++ open_close_principle/virtual_interface_after/InterProcessCommunication/InterProcessCommunication.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <string>
 #include "Logger.h"
+
 class InterProcessCommunication
 {
 public:
   InterProcessCommunication(ILogger& logger_);
+
   void connect(std::string processName);
+
   void sendMessagge(std::string messages);
+
   void receiveMessage(std::string messages);
 private:
   ILogger& logger;
-
 };
